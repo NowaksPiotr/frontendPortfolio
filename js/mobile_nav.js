cece12b8e931aaf00622e360f292c691ae2e0987
Szamponetti <nowak.spiotr@gmail.com>
2021-06-11 20:29:35 +0200

    News

--- js/mobile_nav.js
+++ js/mobile_nav.js
@@ -1,5 +1,7 @@
 let test = document.querySelector(".navigation__mobile-button");
+let tet = document.querySelector(".navigation");
 test.addEventListener('click', klasa);
 function klasa(){
     test.classList.toggle('navigation__mobile-button--on');
+    tet.classList.toggle('navigation--show');
 }
