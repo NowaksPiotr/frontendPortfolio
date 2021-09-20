68f350f174c36f9f10778f6ccd8ece5f4df273a9
Szamponetti <nowak.spiotr@gmail.com>
2021-09-20 14:05:50 +0200

    Added loading cover.

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Variables
+
+let styleSwitchButton = document.querySelector('.styleSwitchButton');
+let loadingCover = document.querySelector('.loading__cover');
+
+
+
+
+styleSwitchButton.addEventListener('click', changeSiteStyle);
+
+
+
+
+function changeSiteStyle(){
+}
