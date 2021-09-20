42b7ebaf4965e20f65cde0e649eed2a160550464
Szamponetti <nowak.spiotr@gmail.com>
2021-09-20 15:56:55 +0200

    Css is almost tidet up and ready.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 let styleSwitchButton = document.querySelector('.styleSwitchButton');
 let loadingCover = document.querySelector('.loading__cover');
-
+let test = document.querySelector('.background__wrapper');
 
 
 
@@ -12,4 +12,5 @@ styleSwitchButton.addEventListener('click', changeSiteStyle);
 
 
 function changeSiteStyle(){
+    test.classList.toggle('background__wrapper--vintage');
 }
