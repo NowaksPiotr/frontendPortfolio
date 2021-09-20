2441491047d8045decc33f29572725914a73ee8e
Szamponetti <nowak.spiotr@gmail.com>
2021-09-20 16:20:46 +0200

    Preparing for writing js for style change.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,30 @@
+/* klasy
+.main-section__wrapper--vintage 3
+.main-section__title--vintage 3
+.internal-link--vintage 2
+.background__wrapper--vintage
+.background__picture--vintage
+.home-content__wrapper--vintage
+ .portrait__wrapper--vintage
+ .portrait__photo-bg--vintage
+  .portrait__photo--vintage
+  .portrait__photo-image--vintage 
+  .home-text__wrapper--vintage
+  .home-text__container--vintage
+  .main-section__title--vintage
+  .header__main--vintage
+  .description-card__wrapper--vintage
+  .description__card--flex--vintage
+  .description-card__wrapper--contact--vintage
+  .projects-examples--vintage
+  .project-example__card--vintage
+   .project-description__buttons--wrapper--vintage
+   .project-description__button--vintage
+   .footer--vintage
+   .footer__wrapper--vintage 
+      .footer__text--vintage
+*/
 // Variables
 
-let styleSwitchButton = document.querySelector('.styleSwitchButton');
-let loadingCover = document.querySelector('.loading__cover');
-let test = document.querySelector('.background__wrapper');
-
-
-
-styleSwitchButton.addEventListener('click', changeSiteStyle);
-
-
-
-
-function changeSiteStyle(){
-    test.classList.toggle('background__wrapper--vintage');
-}
+let test = document.querySelectorAll('.internal-link');
+console.log(test);
