5149800ad7a9508db5811d3091314e6483863afa
Szamponetti <nowak.spiotr@gmail.com>
2021-09-23 14:33:14 +0200

    Working on style switching

--- js/main.js
+++ js/main.js
@@ -1,41 +1,13 @@
-/* Classes
-
-.main-section__wrapper--vintage 3
-.main-section__title--vintage 3
-.internal-link--vintage 2
-.background__wrapper--vintage 1
-.background__picture--vintage 1
-.home-content__wrapper--vintage 1 
- .portrait__wrapper--vintage 1
- .portrait__photo-bg--vintage 1
-  .portrait__photo--vintage 1
-  .portrait__photo-image--vintage  1
-  .home-text__wrapper--vintage 1 
-  .home-text__container--vintage 1
-  .main-section__title--vintage 3
-  .header__main--vintage 3
-  .description-card__wrapper--vintage 1
-  .description__card--flex--vintage 1
-  .main-section__wrapper--contact--vintage 1
-  .projects-examples--vintage 1
-  .project-example__card--vintage 5
-   .project-description__buttons--wrapper--vintage 5
-   .project-description__button--vintage 10
-   .footer--vintage 1
-   .footer__wrapper--vintage 1
-    .footer__text--vintage 1
-
-*/
 // Variables
 
 // DOM Elements that styles will be changed.
 
-let navWrapper = document.querySelector('.nav__wrapper');
-let navigation = document.querySelector('.navigation');
-let navItem = document.querySelectorAll('.navigation__item');
-let navLink = document.querySelectorAll('.navigation__link');
-let navLinkActive = document.querySelector('.navigation__link--active');
-
+let navWrapper = document.querySelector(".nav__wrapper");
+let navigation = document.querySelector(".navigation");
+let navItem = document.querySelectorAll(".navigation__item");
+let navLink = document.querySelectorAll(".navigation__link");
+let navLinkActive = document.querySelector(".navigation__link--active");
+let mobileHamburger = document.querySelector(".navigation__mobile-button");
 let mainSectionWrapper = document.querySelectorAll(".main-section__wrapper");
 let mainSectionTitle = document.querySelectorAll(".main-section__title");
 let internalLink = document.querySelectorAll(".internal-link");
@@ -49,21 +21,26 @@ let portraitPhotoImage = document.querySelector(".portrait__photo-image");
 let homeTextWrapper = document.querySelector(".home-text__wrapper");
 let homeTextContainer = document.querySelector(".home-text__container");
 let headerMain = document.querySelectorAll(".header__main");
-let desctiptionCardWrapper = document.querySelector(".description-card__wrapper");
+let desctiptionCardWrapper = document.querySelector(
+  ".description-card__wrapper"
+);
 let desctiptionCardFlex = document.querySelector(".description__card--flex");
-let mainSectionWrapperConact = document.querySelector(".main-section__wrapper--contact");
+let mainSectionWrapperConact = document.querySelector(
+  ".main-section__wrapper--contact"
+);
 let projectsExamples = document.querySelector(".projects-examples");
 let projectExampleCard = document.querySelectorAll(".project-example__card");
-let projectDescriptionButtonsWrapper = document.querySelectorAll(" .project-description__buttons--wrapper");
-let projectDescriptionButton = document.querySelectorAll(".project-description__button"); 
-let footer = document.querySelector(".footer"); 
-let footerWrapper = document.querySelector(".footer__wrapper"); 
+let projectDescriptionButtonsWrapper = document.querySelectorAll(
+  " .project-description__buttons--wrapper"
+);
+let projectDescriptionButton = document.querySelectorAll(
+  ".project-description__button"
+);
+let footer = document.querySelector(".footer");
+let footerWrapper = document.querySelector(".footer__wrapper");
 let footerText = document.querySelector(" .footer__text");
 
-
-
 //-----------------------------------------------------------------------------
-
 
 let vintage = "--vintage";
 let styleSwitch = document.querySelector(".style-switch-button");
@@ -74,10 +51,25 @@ let styleSwitch = document.querySelector(".style-switch-button");
 styleSwitch.addEventListener("click", styleChange);
 
 function styleChange() {
-  
-  navWrapper.classList.toggle('nav__wrapper'+vintage);
-  navigation.classList.toggle('navigation'+vintage);
-  navLinkActive.classList.toggle('navigation__link--active'+vintage);
+  if (navigation.classList.contains("navigation" + vintage)) {
+    navWrapper.classList.remove("nav__wrapper" + vintage);
+    navWrapper.classList.add('nav__wrapper');
+    navigation.classList.remove('navigation' + vintage);
+    navigation.classList.add("navigation");
+    // navLinkActive.classList.remove("navigation__link--active" + vintage);
+    // navLinkActive.classList.add("navigation__link--active");
+    mobileHamburger.classList.remove("navigation__mobile-button" + vintage);
+    mobileHamburger.classList.add("navigation__mobile-button");
+  } else {
+    navWrapper.classList.remove("nav__wrapper");
+    navWrapper.classList.add("nav__wrapper" + vintage);
+    navigation.classList.remove("navigation");
+    navigation.classList.add("navigation" + vintage);
+    // navLinkActive.classList.remove("navigation__link--active");
+    // navLinkActive.classList.add("navigation__link--active" + vintage);
+    mobileHamburger.classList.remove("navigation__mobile-button");
+    mobileHamburger.classList.add("navigation__mobile-button" + vintage);
+  }
 
   backgroundWrapper.classList.toggle("background__wrapper" + vintage);
   backgroundPicture.classList.toggle("background__picture" + vintage);
@@ -87,14 +79,18 @@ function styleChange() {
   portraitPhoto.classList.toggle("portrait__photo" + vintage);
   portraitPhotoImage.classList.toggle("portrait__photo-image" + vintage);
   homeTextContainer.classList.toggle("home-text__container" + vintage);
-  desctiptionCardWrapper.classList.toggle("description-card__wrapper" + vintage);
+  desctiptionCardWrapper.classList.toggle(
+    "description-card__wrapper" + vintage
+  );
   desctiptionCardFlex.classList.toggle("description__card--flex" + vintage);
-  mainSectionWrapperConact.classList.toggle("main-section__wrapper--contact" + vintage);
+  mainSectionWrapperConact.classList.toggle(
+    "main-section__wrapper--contact" + vintage
+  );
   projectsExamples.classList.toggle("projects-examples" + vintage);
   homeTextWrapper.classList.toggle("home-text__wrapper" + vintage);
-  footer.classList.toggle('footer'+vintage);
-  footerWrapper.classList.toggle('footer__wrapper'+vintage);
-  footerText.classList.toggle('footer__text'+vintage);
+  footer.classList.toggle("footer" + vintage);
+  footerWrapper.classList.toggle("footer__wrapper" + vintage);
+  footerText.classList.toggle("footer__text" + vintage);
 
   mainSectionWrapper.forEach((el, index) => {
     mainSectionWrapper[index].classList.toggle(
@@ -115,16 +111,20 @@ function styleChange() {
       "project-example__card" + vintage
     );
   });
-  projectDescriptionButtonsWrapper.forEach((el, index)=>{
-    projectDescriptionButtonsWrapper[index].classList.toggle('project-description__buttons--wrapper'+vintage);
+  projectDescriptionButtonsWrapper.forEach((el, index) => {
+    projectDescriptionButtonsWrapper[index].classList.toggle(
+      "project-description__buttons--wrapper" + vintage
+    );
   });
-  projectDescriptionButton.forEach((el, index)=>{
-    projectDescriptionButton[index].classList.toggle('project-description__button'+vintage);
+  projectDescriptionButton.forEach((el, index) => {
+    projectDescriptionButton[index].classList.toggle(
+      "project-description__button" + vintage
+    );
   });
-  navItem.forEach((el, index)=>{
-    navItem[index].classList.toggle('navigation__item'+vintage);
+  navItem.forEach((el, index) => {
+    navItem[index].classList.toggle("navigation__item" + vintage);
   });
-  navLink.forEach((el, index)=>{
-    navLink[index].classList.toggle('navigation__link'+vintage);
-  })
+  navLink.forEach((el, index) => {
+    navLink[index].classList.toggle("navigation__link" + vintage);
+  });
 }
--- js/mobile_nav.js
+++ js/mobile_nav.js
@@ -7,38 +7,59 @@ let mobileNavBtn = document.querySelector(".navigation__mobile-button");
 
 let menuLink = document.querySelectorAll(".navigation__link");
 // **** HAMBURGER MENU **** //
-
 // Toggle function to open hamburger menu
 
 mobileNavBtn.addEventListener("click", hamburgerToggle);
+// switchButton.addEventListener("click", vintageCheck);
 
 function hamburgerToggle() {
-  mobileNavBtn.classList.toggle("navigation__mobile-button--on");
-  mobileNavList.classList.toggle("navigation--show");
+  if (mobileNavBtn.classList.contains("navigation__mobile-button--vintage")) {
+    mobileNavBtn.classList.toggle("navigation__mobile-button--on--vintage");
+    mobileNavList.classList.toggle("navigation--show--vintage");
+  } else {
+    mobileNavBtn.classList.toggle("navigation__mobile-button--on");
+    mobileNavList.classList.toggle("navigation--show");
+  }
 }
 
+// function vintageCheck() {
+//   if (isVintage == false) {
+//     isVintage = true;
+//     menuLink.forEach(function activeItem(link, index) {
+//       link.addEventListener("click", () => {
+//         let activeLink = document.querySelector(".navigation__link--active--vintage");
+//         if (activeLink) {
+//           activeLink.classList.remove("navigation__link--active--vintage");
+//         }
+//         menuLink[index].classList.add("navigation__link--active--vintage");
+//       });
+//     });
+//     return isVintage;
+//   }
+//   isVintage = false;
+//   return isVintage;
+// }
 // After click on hamburger menu link hamburger menu will hide and user will be taken to desired element.
 
 hamburgerMenuLink.forEach(function toggler(link, index) {
   link.addEventListener("click", toggleAndTake);
 
   function toggleAndTake() {
-      mobileNavBtn.classList.remove("navigation__mobile-button--on");
-      mobileNavList.classList.remove("navigation--show");
-}});
+    mobileNavBtn.classList.remove("navigation__mobile-button--on");
+    mobileNavList.classList.remove("navigation--show");
+  }
+});
 
 //********************************* //
 
 // Function for active menu
-
-menuLink.forEach(function activeItem(link, index){
-  link.addEventListener("click", ()=>{
-    let activeLink = document.querySelector(".navigation__link--active");
-    if(activeLink){
-      activeLink.classList.remove("navigation__link--active");
-    }
-    menuLink[index].classList.add("navigation__link--active");
-  })
-})
-
+  menuLink.forEach(function activeItem(link, index) {
+    link.addEventListener("click", () => {
+      let activeLink = document.querySelector(".navigation__link--active");
+      if (activeLink) {
+        activeLink.classList.remove("navigation__link--active");
+      }
+      menuLink[index].classList.add("navigation__link--active");
+    });
+  });
 //********************************* //
