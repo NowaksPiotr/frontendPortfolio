884710b9dc86ad61a3cc8899574671f35f218cb7
Szamponetti <nowak.spiotr@gmail.com>
2021-09-24 12:35:22 +0200

    Don't remember what changed

--- js/main.js
+++ js/main.js
@@ -1,15 +1,42 @@
-// Variables
+// ** Variavbles for hamburger menu
+let mobileNavList = document.querySelector(".navigation");
+let hamburgerMenuLink = document.querySelectorAll(".navigation__link");
+let mobileNavBtn = document.querySelector(".navigation__mobile-button");
+// **
 
-let styleSwitchButton = document.querySelector('.styleSwitchButton');
-let loadingCover = document.querySelector('.loading__cover');
+let menuLink = document.querySelectorAll(".navigation__link");
+// **** HAMBURGER MENU **** //
 
+// Toggle function to open hamburger menu
 
+mobileNavBtn.addEventListener("click", hamburgerToggle);
 
-
-styleSwitchButton.addEventListener('click', changeSiteStyle);
-
-
-
-
-function changeSiteStyle(){
+function hamburgerToggle() {
+  mobileNavBtn.classList.toggle("navigation__mobile-button--on");
+  mobileNavList.classList.toggle("navigation--show");
 }
+
+// After click on hamburger menu link hamburger menu will hide and user will be taken to desired element.
+
+hamburgerMenuLink.forEach(function toggler(link, index) {
+  link.addEventListener("click", toggleAndTake);
+
+  function toggleAndTake() {
+      mobileNavBtn.classList.remove("navigation__mobile-button--on");
+      mobileNavList.classList.remove("navigation--show");
+}});
+
+//********************************* //
+
+// Function for active menu
+
+menuLink.forEach(function activeItem(link, index){
+  link.addEventListener("click", ()=>{
+    let activeLink = document.querySelector(".navigation__link--active");
+    if(activeLink){
+      activeLink.classList.remove("navigation__link--active");
+    }
+    menuLink[index].classList.add("navigation__link--active");
+  })
+})
+//********************************* //
